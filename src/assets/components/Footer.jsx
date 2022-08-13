f0f6f9d9386a94abd397ca0d7a02ed7e1c71ee2c
Fernando Diaz <70718664+aboueidlautaro@users.noreply.github.com>
2022-08-13 00:37:20 -0300

    update background video

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full h-96 bg-grafito flex justify-center flex-col text-center items-center">
+    <footer className="w-full h-96 bg-black flex justify-center flex-col text-center items-center text-hueso">
       <div className="p-12 text-center h-full w-11/12">
         <h3>LOGO</h3>
         <span>Copyright &copy; 2022 Lenny </span>
--- src/assets/routes/Home.jsx
+++ src/assets/routes/Home.jsx
@@ -1,20 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "../static/js/btnAnimate";
-import video1 from "../static/images/video1.mp4";
+import video from "../static/images/video.webm";
 import video2 from "../static/images/video2.mp4";
 
 function Home() {
-  const [videoPlay, setVideoPlay] = useState(video1);
-
   const vidRef = useRef();
   useEffect(() => {
     vidRef.current.play();
   }, []);
-
-  const handleVideoEnded = () => {
-    videoPlay === video1 ? setVideoPlay(video2) : setVideoPlay(video1);
-  };
 
   return (
     <>
@@ -22,11 +16,11 @@ function Home() {
         <video
           ref={vidRef}
           muted
+          loop
           autoPlay
           className="w-full absolute top-0 z-index 10 h-screen object-cover "
-          src={videoPlay}
+          src={video}
           alt=""
-          onEnded={handleVideoEnded}
         />
         <div
           id="container-home"
@@ -44,7 +38,7 @@ function Home() {
                 server
               </span>
             </h2>
-            <p className="w-96 sm:w-11/12 mt-8 text-grafito-light">
+            <p className="w-96 sm:w-11/12 mt-8 font-medium text-white/80 font-roboto">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
               dolorem provident doloribus cupiditate! Eius perspiciatis
               reiciendis aperiam amet facere dignissimos?
@@ -60,7 +54,8 @@ function Home() {
         </div>
       </div>
 
-      <div className="relative h-screen bg-gradient-to-t from-[#8428C4] to-black "></div>
+      <div className="relative h-screen bg-gradient-to-t from-[#621c93] to-black "></div>
+      <div className="relative h-screen bg-gradient-to-t from-black  to-[#621c93] "></div>
     </>
   );
 }
